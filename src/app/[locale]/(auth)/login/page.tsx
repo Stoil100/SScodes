20b2c4f9a44f4878e9e336b53a2f38b085c1aa41
Stoil100 <toli100ilov@gmail.com>
2024-07-09 10:46:37 +0300

    Chore: Update auth pages design

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -8,8 +8,18 @@ export default async function LoginPage() {
         redirect("/profile");
     }
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <AuthForm variant="login" />
-        </main>
+        <main className="flex h-screen items-center justify-center">
+        <div className="relative flex h-full  w-full flex-col items-center justify-center space-y-5 px-10 drop-shadow-lg md:w-1/2 md:p-0">
+            <div className="z-10 w-full md:w-2/3">
+                <AuthForm variant="login" />
+            </div>
+        </div>
+        <div className="relative hidden h-full w-1/2 flex-col items-center justify-center gap-8 overflow-hidden md:flex ">
+            <img
+                className="absolute right-0 h-max min-h-screen overflow-hidden object-cover"
+                src="/auth/pattern.png"
+            />
+        </div>
+    </main>
     );
 }
